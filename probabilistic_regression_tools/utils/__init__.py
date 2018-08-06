d6f5e2c59d25f45cf200735bc41188221bf610b5
Jens Schreiber <jens.schreiber@uni-kassel.de>
2018-08-06 13:36:56 +0200

    added function to convert quantiles to scipy rv_continous

--- probabilistic_regression_tools/utils/__init__.py
+++ probabilistic_regression_tools/utils/__init__.py
@@ -1,2 +1,3 @@
 from .distributions import probdists_2_quantiles
+from .distributions import quantiles_2_probdists
 from .plot import plot_quantile_forecast
--- probabilistic_regression_tools/utils/distributions.py
+++ probabilistic_regression_tools/utils/distributions.py
@@ -1,6 +1,8 @@
 """ Conversion from scipy.stats rv_continuous to quantiles. """
 
 import numpy as np
+from scipy import interpolate
+from scipy.stats import rv_continuous
 
 __author__ = "Jens Schreiber"
 __copyright__ = "Copyright 2018"
@@ -30,4 +32,45 @@ def probdists_2_quantiles(probabilistic_forecasts, quantiles=np.linspace(0.1, 0.
     return np.array(quantile_forecasts)
 
 
-    ## TODO should also provide the other way around
+class Quantile_2_CDF(rv_continuous):
+    def stats(self):
+        return 0., 0.
+
+    def _init(self, x_values, quantiles):
+        self.x_values = x_values
+        self.quantiles = quantiles
+
+    def _cdf(self, x):
+        """USE self.quantiles and x_values to return correct values. Ideally via linear interpolation"""
+        return np.interp(x, self.x_values, self.quantiles)
+
+
+def quantiles_2_probdists(quantile_forecasts, quantiles):
+    """
+        Converts list of 'M' quantile forecasts to scipy.stats objects (rv_continuous)
+        representation.
+
+        Parameters
+        ----------
+            quantile_forecasts: array, shape(M,Q)
+                The quantile forecats.
+              quantiles: array_like
+                "Q" values of the quantiles to be evaluated,
+                e.g., numpy array with 'Q' elements in range [0..1].
+        Returns
+        -------
+            probabilistic_forecasts: list
+                The resulting List of "M" scipy.stats.rv_continuous distributions
+                https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.rv_continuous.html
+    """
+    N = np.array(quantile_forecasts).shape[0]
+    result = []
+
+    for i in range(N):
+        cur_cdf = Quantile_2_CDF(name='mycdf')
+        cur_cdf.x_values = quantile_forecasts[i, :]
+        cur_cdf.quantiles = quantiles
+        result.append(cur_cdf)
+
+    return np.array(result)
+
